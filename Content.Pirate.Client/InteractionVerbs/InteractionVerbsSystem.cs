cbe8ef00f5fba00a934d0d2860239736a1bf0d57
v0id <>
2025-10-21 15:47:24 +0300

    Remove EE interactions verb due to Goob port it

--- Content.Pirate.Client/InteractionVerbs/InteractionVerbsSystem.cs
+++
@@ -1,8 +0,0 @@
-using Content.Pirate.Shared.InteractionVerbs;
-
-namespace Content.Pirate.Client.InteractionVerbs;
-
-// Just here because the shared system is abstract.
-public sealed class InteractionVerbsSystem : SharedInteractionVerbsSystem
-{
-}
